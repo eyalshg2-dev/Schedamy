b9b95e05f4084f01bc0a4f9505a6788a164efb6b
User <User@DESKTOP-LVDF8B3>
2026-05-15 17:20:38 +0300

    גרסא ראשונית. - אוראל -

--- Schedamy/src/Schedamy/Lesson.java
+++ Schedamy/src/Schedamy/Lesson.java
@@ -1,5 +1,26 @@
 package Schedamy;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class Lesson {
 
+	private LocalDate lessonDate;
+	private LocalTime startTime;
+	private LocalTime endTime;
+	
+	public LocalDate getLessonDate()
+	{
+	    return lessonDate;
+	}
+
+	public LocalTime getStartTime()
+	{
+	    return startTime;
+	}
+
+	public LocalTime getEndTime()
+	{
+	    return endTime;
+	}
 }
