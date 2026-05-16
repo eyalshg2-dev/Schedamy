50e4ef230385edd0cc9abfbe9661f7beb395bb2a
User <User@DESKTOP-LVDF8B3>
2026-05-17 00:27:53 +0300

    /

---
+++ Schedamy/src/Schedamy/Course.java
@@ -0,0 +1,85 @@
+package Schedamy;
+
+
+import java.util.Vector;
+
+public class Course{
+	
+	private int courseID;
+	private String courseName;
+	private int credits;
+	private Vector<Lesson> lessons;
+	private String courseType;
+	
+	 
+	public Course(int courseID, String courseName, int credits, String courseType) {
+		this.courseID = courseID;
+		setCourseName(courseName);
+		setCredits(credits);
+		setCourseType(courseType);
+	}
+	
+	// calculate the total hours by iterating through the lessons
+	public double calculateCourseHours() {
+		double totalHours = 0;
+		
+		for (Lesson lesson : lessons) {
+			totalHours += lesson.getDurationTime().toMinutes();
+		}
+		
+		return totalHours;
+	}
+	
+	
+	public int getCourseID() {
+		return this.courseID;
+	}
+	
+	
+	public String getCourseName() {
+		return this.courseName;
+	}
+	
+	
+	public void setCourseName(String courseName) {
+		this.courseName = courseName;
+	}
+	
+	
+	public int getCredits() {
+		return this.credits;
+	}
+	
+	
+	public void setCredits(int credits) {
+		this.credits = credits;
+		
+	}
+	
+	
+	public String getCourseType() {
+		return this.courseType;
+	}
+	
+	public Vector<Lesson> getLessons(){
+		return this.lessons;
+	}
+	
+	public void setCourseType(String courseType) {
+		if (courseType.equals("elective") || 
+			courseType.equals("mandatory")) {
+			this.courseType = courseType;
+		} else {
+			throw new IllegalArgumentException("No valid course types");
+		}
+	}
+	
+	
+	public void addLesson (Lesson lesson) {
+		if (lesson == null) {
+			throw new IllegalArgumentException("Lesson cannot be null");
+		}
+		
+		 lessons.add(lesson);
+	}
+}
