a9081e530435d4119acf13b9fb426043b7f24164
User <User@DESKTOP-LVDF8B3>
2026-05-17 00:25:49 +0300

    .

---
+++ Schedamy/src/Schedamy/Lecturer.java
@@ -0,0 +1,88 @@
+package Schedamy;
+
+import java.util.ArrayList;
+
+public class Lecturer {
+
+	private int lecturerID;
+	private String firstName;
+	private String lastName; 
+	private ArrayList<String> specializations ;
+	private double teachingScore;  
+	private double FTE;
+	private static final int FULL_TIME_HOURS = 40;
+	
+	//Constructor
+	public Lecturer(int lecturerID, String firstName, String lastName, ArrayList<String> specializations, double teachingScore, double FTE)
+		{
+			this.lecturerID = lecturerID;
+			this.firstName = firstName;
+			this.lastName = lastName;
+			this.specializations = specializations;
+			this.teachingScore = teachingScore;
+			this.FTE = FTE;
+		}	
+	// get lecturerId
+	public int getLecturerID()
+	{
+		return this.lecturerID;
+	}
+	
+	// get firstName
+	public String getFirstName()
+	{
+		return this.firstName;
+	}
+	
+	// get lastName
+	public String getLastName()
+	{
+		return this.lastName;
+	}
+	
+	// get specialization
+	public ArrayList<String> getSpecializations()
+	{
+		return this.specializations;
+	}
+	
+	// get teachingScore
+	public double getTeachingScore()
+	{
+		return this.teachingScore;
+	}
+	
+	// get FTE
+	public double getFTE()
+	{
+		return this.FTE;
+	}
+	    
+	//To String
+	public String toString() 
+	{
+		return "		The Lecturer is: " + firstName + " " + lastName + " "  + lecturerID + "\n" +
+			   "        Specializations: " + specializations + "\n " + 
+			   "		FTE: " + FTE + "\n" + 
+			   "		Teaching Score: " + teachingScore;
+	}
+	
+	// calculate how many hours difference exists
+	public double calculateWeeklyLoadDiff()
+	{
+		double requiredHours = FTE*FULL_TIME_HOURS;
+		double actualHours = calculateLecturerWeeklyHours();
+		return requiredHours-actualHours;
+	}	
+	
+	private double calculateLecturerWeeklyHours() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	//  did Lecturer meet required workload?
+	public boolean hasRequiredHours()
+	{
+			return (calculateWeeklyLoadDiff()<=0);
+			
+	}
+}
--- Schedamy/src/Schedamy/Lesson.java
+++ Schedamy/src/Schedamy/Lesson.java
@@ -1,26 +1,125 @@
 package Schedamy;
-
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 public class Lesson {
-
+	
+	private int lessonID;
 	private LocalDate lessonDate;
 	private LocalTime startTime;
 	private LocalTime endTime;
+	private String status;
+	private String teachingMode;
+	private boolean requiresLabRoom;
+
 	
-	public LocalDate getLessonDate()
-	{
-	    return lessonDate;
+	public Lesson(int lessonID, LocalDate lessonDate, LocalTime startTime,
+		  		  LocalTime endTime, String status,
+		  		  String teachingMode, boolean requiresLabRoom) {
+		
+		if (lessonDate == null || startTime == null || endTime == null ||
+			status == null) {
+			throw new IllegalArgumentException("Fields cannot be null");
+		}
+		
+		if (!endTime.isAfter(startTime)) {
+			throw new IllegalArgumentException("End time must be after start time");
+		}
+		
+	this.lessonID = lessonID;
+	this.lessonDate = lessonDate;
+	this.startTime = startTime;
+	this.endTime = endTime;
+	this.requiresLabRoom = requiresLabRoom;
+	setStatus(status);
+	setTeachingMode(teachingMode);
+	}
+			
+	public int getLessonID() {
+		return this.lessonID;
+	}
+	
+	public LocalDate getLessonDate() {
+		return this.lessonDate;
+	}
+	
+	public LocalTime getStartTime() {
+		return this.startTime;
+	}
+	
+	public LocalTime getEndTime() {
+		return this.endTime;
+	}
+	
+	public String getStatus() {
+		return this.status;
+	}
+	
+	public void setStatus(String status) {
+		
+		if (status == null) {
+			throw new IllegalArgumentException("Status cannot be null");
+		}
+		
+		if (status.equals("SCHEDULED")||
+			status.equals("RESCHEDULED")||
+			status.equals("CANCELLED")) {
+			
+			this.status = status;
+			
+		} else {
+			throw new IllegalArgumentException("Invalid status");
+		}
+	}
+	
+	
+	public Duration getDurationTime() {
+		
+		return Duration.between(getStartTime(), getEndTime());
 	}
 
-	public LocalTime getStartTime()
-	{
-	    return startTime;
+	
+	public String getTeachingMode() {
+		return this.teachingMode;
+	}
+	
+	
+	public void setTeachingMode(String teachingMode) {
+		
+		if (teachingMode == null) {
+			throw new IllegalArgumentException ("Teaching mode cannot be null");
+		}
+		
+		if (teachingMode.equals("FRONTAL") ||
+			teachingMode.equals("ZOOM") ||
+			teachingMode.equals("HYBRID")) {
+			this.teachingMode = teachingMode;
+		}
+	}
+	
+	
+	public boolean getRequiresLabRoom() {
+		
+		return this.requiresLabRoom;
 	}
 
-	public LocalTime getEndTime()
-	{
-	    return endTime;
-	}
+	
+	public void changeMode(String newMode) {
+		
+		if (newMode == null) {
+			throw new IllegalArgumentException("Teaching mode cannot be null");
+		}
+		
+		if (newMode.equals("FRONTAL") ||
+			newMode.equals("ZOOM") ||
+			newMode.equals("HYBRID")){
+			
+			this.teachingMode = newMode;
+			
+		} else {
+			throw new IllegalArgumentException("Invalid teaching mode");
+		}
+	}		
 }
+	
