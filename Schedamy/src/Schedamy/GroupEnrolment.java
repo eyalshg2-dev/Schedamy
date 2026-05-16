c3bca98bb2b95438cfb7f53cca5cb3adcda6cc69
User <User@DESKTOP-LVDF8B3>
2026-05-16 23:48:30 +0300

    First version GET and methods

--- Schedamy/src/Schedamy/GroupEnrolment.java
+++ Schedamy/src/Schedamy/GroupEnrolment.java
@@ -74,5 +74,4 @@ public class GroupEnrolment
         priorityLevel = finalPriority;
         return priorityLevel;
     }
-
 }
--- Schedamy/src/Schedamy/StudentGroup.java
+++ Schedamy/src/Schedamy/StudentGroup.java
@@ -108,6 +108,7 @@ public class StudentGroup
     	System.out.println("The group schedule is not overloaded");
     	return false;
     }
+    
 }
 
 	
